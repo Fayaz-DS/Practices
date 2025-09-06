8baf61fe77ec1530ad388b53766aed22e8a10a12
Syed Fayaz Mehdi <userc5630@gmail.com>
2025-09-07 00:39:34 +0530

    Add Calci

---
+++ calculator.py
@@ -0,0 +1,18 @@
+a = 5
+b = 1
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def exponent(a, b):
+    return a ** b
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))       
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
+print("Exponent:", exponent(a, b))
