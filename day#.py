200e80fb16b43fb4eea06d5384c04236a46554e3
Syed Fayaz Mehdi <userc5630@gmail.com>
2025-09-28 20:58:03 +0530

    New p commit

---
+++ day#.py
@@ -0,0 +1,30 @@
+# class Student: 
+#   def __init__(self, name, year, enrolled, gpa):
+#     self.name = name
+#     self.year = year
+#     self.enrolled = enrolled
+#     self.gpa = gpa
+  
+#   def display_info(self):
+#     print('The student ' + self.name + '\'s GPA is ' + str(self.gpa) + '!')
+
+# mitsuha = Student('Mitsuha', 8, True, 3.8)
+# taki = Student('Taki', 9, True, 3.6)
+
+# mitsuha.display_info()
+# taki.display_info()
+
+# class Student: 
+#   def __init__(self, name, year, enrolled, gpa):
+#     self.name = name
+#     self.year = year
+#     self.enrolled = enrolled
+#     self.gpa = gpa
+  
+#   def display_info(self):
+#     print('The student ' + self.name + '\'s is ' + str(self.gpa) + '!')
+  
+#   def graduation(self):
+#     if self.enrolled and self.gpa > 2.5 and self.year == 12:
+#       print(self.name + ' will be able to graduate this year!')
+
