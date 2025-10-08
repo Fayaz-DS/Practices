98d3e46ef1672122011a8760249f1d2887b875fc
Fayaz Mehdi <userc5630@gmail.com>
2025-10-08 14:36:30 +0530

    Pizzaaaa

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+student_info = {'name': 'Alice', 'age': 9, 'grade': 'A'}
+
+print('Name:', student_info['name'])
+print('Age:', student_info['age'])
+print('Grade:', student_info['grade'])
+
+student_info['age'] = 10
+print('Updated Age:', student_info['age'])
+# Dictionary methods
+print('Keys:', student_info.keys())
+print('Values:', student_info.values())
+print('Items:', student_info.items())
+
+Black_Light = {'Artist' : 'Sheryl Oppenheim', 'Period' : '1983', 'Date' : '2014'}
+
+print(Black_Light.keys())
+print(Black_Light.values())
+print(Black_Light.items())
---
+++ l&l.py
@@ -0,0 +1,11 @@
+city_1 = ("Hyderabad",)
+friend_1 = ("shik", 19.075983, 72.877655)
+friend_2 = ("nik", 28.613939, 77.209021)
+friend_3 = ("vik", 12.971599, 77.594566)
+
+print(friend_1)
+print(friend_2)
+print(friend_3)
+
+all_friends = friend_1 + friend_2 + friend_3
+print(all_friends)
